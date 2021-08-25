ec64b2291dd3071e3ea3185cd8a9c270f7f4a7b5
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-25 05:21:43 -0300

    Iniciando a minha estrutura

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, NextScript, Main } from "next/document";
+
+export class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
