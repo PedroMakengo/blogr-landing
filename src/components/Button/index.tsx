1708823f753fa352e0c5a470f1256f8fc6be15ef
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-25 06:19:18 -0300

    Atualizando

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,9 @@
 import style from "./style.module.scss";
 
 export function Button({ props }) {
-  return <button onClick={props.onclick}>{props.title}</button>;
+  return (
+    <button onClick={props.onclick} className={style.button}>
+      {props.title}
+    </button>
+  );
 }
