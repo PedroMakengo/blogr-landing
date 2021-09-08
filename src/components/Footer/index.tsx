c296715146f5538e06e65c6f290bf44fd52ff648
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-08 08:03:04 -0300

    Terminando o projeto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,8 +4,21 @@ export function Footer() {
   return (
     <footer className={style.footer}>
       <div className={style.container}>
-        <img src="../../../images/logo.svg" alt="" />
-        <div>Menu</div>
+        <div>
+          <img src="../../../images/logo.svg" alt="" />
+        </div>
+        <div className={style.menuFooter}>
+          <div className={style.itemFooter}>
+            <h2>Product</h2>
+            <nav>
+              <a href="#">Overview</a>
+              <a href="#">Pricing</a>
+              <a href="#">Marketplace</a>
+              <a href="#">Features</a>
+              <a href="#">Integratios</a>
+            </nav>
+          </div>
+        </div>
       </div>
     </footer>
   );
